6d2ad2c9d5bb4463f2529d5a3fa03bf230469f67
Caio Henrique <128559635+chaltech@users.noreply.github.com>
2024-03-03 17:48:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function processar(acao) {
+  var inputText = document.getElementById('inputText').value;
+  var resultado;
+
+  if (acao === 'criptografar') {
+    resultado = criptografarTexto(inputText);
+  } else if (acao === 'descriptografar') {
+    resultado = descriptografarTexto(inputText);
+  }
+
+  document.getElementById('resultado').innerText = resultado;
+}
+
+function copiarResultado() {
+  var resultado = document.getElementById('resultado').innerText;
+  navigator.clipboard.writeText(resultado)
+    .then(() => alert('Resultado copiado com sucesso!'))
+    .catch(err => console.error('Erro ao copiar resultado: ', err));
+}
+
+
+function criptografarTexto(texto) {
+  
+  return texto.split('').reverse().join('');
+}
+
+
+function descriptografarTexto(texto) {
+  
+  return texto.split('').reverse().join('');
+}
